2666fc9121a48005c249e6ffb462f5a5fba9e536
Ilias <igoujgali@gmail.com>
2020-03-25 14:35:41 +0100

    #16 p.51

--- src/main/Money.java
+++ src/main/Money.java
@@ -15,19 +15,20 @@ public class Money {
     public String currency() {
         return currency;
     }
-
     public static Money dollar(int amount) {
         return new Money(amount, "USD");
     }
     public static Money franc(int amount) {
         return new Money(amount, "CHF");
     }
-
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && currency().equals(money.currency());
     }
     public String toString() {
         return amount + " " + currency;
+    }
+    public Money plus(Money addend) {
+        return  new Money(amount + addend.amount, currency);
     }
 }
