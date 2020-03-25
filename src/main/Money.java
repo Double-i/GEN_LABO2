5bed16a17ed4bdab4cf66e30788dac0bfeb8e8fa
Ilias <igoujgali@gmail.com>
2020-03-25 13:07:07 +0100

    #14 p.39

--- src/main/Money.java
+++ src/main/Money.java
@@ -6,11 +6,15 @@ public abstract class Money {
     public static Money dollar(int amount) {
         return new Dollar(amount);
     }
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
     public abstract Money times(int multiplier);
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
+
 
 }
