7edbaa050b0706cb2c4e4291352edd512844b36c
Ilias <igoujgali@gmail.com>
2020-03-25 13:12:34 +0100

    #13 p.41

--- src/main/Dollar.java
+++ src/main/Dollar.java
@@ -11,6 +11,12 @@ public class Dollar extends Money {
 
         return new Dollar( amount * multiplier) ;
     }
+
+    @Override
+    public String currency() {
+        return "USD";
+    }
+
     public boolean equals(Object object) {
         Money dollar= (Dollar) object;
         return amount == dollar.amount;
--- src/main/Money.java
+++ src/main/Money.java
@@ -3,13 +3,15 @@ package main;
 public abstract class Money {
     protected int amount;
 
+    public abstract Money times(int multiplier);
+    public abstract String currency();
+
     public static Money dollar(int amount) {
         return new Dollar(amount);
     }
     public static Money franc(int amount) {
         return new Franc(amount);
     }
-    public abstract Money times(int multiplier);
 
     public boolean equals(Object object) {
         Money money = (Money) object;
