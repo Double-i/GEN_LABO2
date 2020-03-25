5c7c2f61bbf4eabf701fe192a1b4103c8c44f5d7
Ilias <igoujgali@gmail.com>
2020-03-25 12:27:54 +0100

    #11 p.34

--- src/main/Money.java
+++ src/main/Money.java
@@ -2,4 +2,8 @@ package main;
 
 public class Money {
     protected int amount;
+    public boolean equals(Object object) {
+        Money money= (Money) object;
+        return amount == money.amount;
+    }
 }
