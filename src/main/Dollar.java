f5496bcdab3ffcbcef172f4573aa1cd7627f0bd0
Ilias <igoujgali@gmail.com>
2020-03-25 13:48:00 +0100

    fixes #13 p.44

--- src/main/Dollar.java
+++ src/main/Dollar.java
@@ -3,8 +3,7 @@ package main;
 public class Dollar extends Money {
 
     public Dollar(int amount, String currency) {
-        this.amount = amount;
-        this.currency = currency;
+        super(amount, currency);
     }
     public Money times(int multiplier) {
         return Money.dollar(amount * multiplier);
--- src/main/Franc.java
+++ src/main/Franc.java
@@ -2,8 +2,7 @@ package main;
 
 public class Franc extends Money {
     public Franc(int amount, String currency) {
-        this.amount= amount;
-        this.currency = currency;
+        super(amount, currency);
     }
     public Money times(int multiplier) {
         return Money.franc(amount * multiplier);
